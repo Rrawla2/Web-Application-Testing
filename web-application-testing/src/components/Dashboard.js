7ff9c62ab21d2a2d107d43101eee1ccc671b7ab5
Kristi Gribble <kgribble4083@gmail.com>
2020-01-24 11:26:53 -0600

    tested function with jest

--- web-application-testing/src/components/Dashboard.js
+++ web-application-testing/src/components/Dashboard.js
@@ -9,10 +9,12 @@ const Dashboard = () => {
   // balls and strikes reset to 0 when a `hit` is recorded.
   // a `foul` increases strikes up to 2. With no strikes, a foul makes it 1 strike. With 1 strike, a foul makes it 2 strikes. With two strikes a foul has no effect, count stays at 2 strikes.
   // a hit should make all reset to 0
+    
+
     const sum = (num1, num2) => {
         return num1 + num2
     }
-
+    
     const Strikes = () => {
         if (strikes < 2) {
             return setStrikes(sum(strikes, 1))
