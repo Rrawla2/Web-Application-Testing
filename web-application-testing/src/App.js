dab829daae85b88fe82e28f76596d6e4beb52f1d
Kristi Gribble <kgribble4083@gmail.com>
2020-01-23 22:11:18 -0600

    added basic react to display and dashboard files

--- web-application-testing/src/App.js
+++ web-application-testing/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Display from "./components/Display";
+import Dashboard from "./components/Dashboard"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>Baseball at Bat Tracker</h1>
       <Display />
       <Dashboard />
       </header>
